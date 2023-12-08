bc57d2a7827d8ad199ed7b0b66c8e61f60104d8b
Goo-hyunwoo <gusdnsladl@gmail.com>
2023-12-08 16:45:41 +0900

    [2023-12-08] 로그 필터 생성

---
+++ api/src/main/java/org/delivery/api/filter/LoggerFilter.java
@@ -0,0 +1,63 @@
+package org.delivery.api.filter;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.util.ContentCachingRequestWrapper;
+import org.springframework.web.util.ContentCachingResponseWrapper;
+
+import java.io.IOException;
+
+@Slf4j
+@Component
+public class LoggerFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+
+        var req = new ContentCachingRequestWrapper((HttpServletRequest) request);
+        var res = new ContentCachingResponseWrapper((HttpServletResponse) response);
+        
+        // req(Header, Body)를 doFilter 이전에 실행하기 위해서는 별도의 HttpServletRequest를 만들어서 써야하지만, 강의에서는 그 수준에서는 하지 않음
+
+        chain.doFilter(req, res);
+
+        //request 정보
+        var headerNames = req.getHeaderNames();
+        var headerValues = new StringBuilder();
+        headerNames.asIterator().forEachRemaining(headerKey -> {
+            var headerValue = req.getHeader(headerKey);
+            headerValues
+                    .append("[")
+                    .append(headerKey)
+                    .append(":")
+                    .append(headerValue)
+                    .append("] ");
+        });
+
+        var requestBody = new String(req.getContentAsByteArray());
+        var uri = req.getRequestURI();
+        var method = req.getMethod();
+        log.info(">>> uri : {}, method: {},  header : {} \r\n body : {}",uri, method, headerValues, requestBody);
+
+        //response 정보
+        var responseHeaderValues = new StringBuilder();
+        res.getHeaderNames().forEach(headerKey -> {
+            var headerValue = req.getHeader(headerKey);
+            responseHeaderValues
+                    .append("[")
+                    .append(headerKey)
+                    .append(":")
+                    .append(headerValue)
+                    .append("] ");
+        });
+
+        var responseBody = new String(res.getContentAsByteArray());
+        log.info(">>> uri : {}, method: {},  header : {} \r\n body : {}",uri, method ,responseHeaderValues, responseBody);
+
+        //response 다시 세팅
+        res.copyBodyToResponse();
+
+    }
+}
