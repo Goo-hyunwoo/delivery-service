773a2bbdc5706fee568b7027883839b9dbd3a023
Goo-hyunwoo <gusdnsladl@gmail.com>
2023-12-15 11:33:36 +0900

    [2023-12-15] JWT 토큰 추가, JWT 토큰 생성/검증/에러 추가

---
+++ api/src/main/java/org/delivery/api/common/error/TokenErrorCode.java
@@ -0,0 +1,17 @@
+package org.delivery.api.common.error;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum TokenErrorCode implements ErrorCodeInf{
+    INVALID_TOKEN(400, 2000, "유효하지 않은 토큰"),
+    EXPIRED_TOKEN(400, 2001, "만료된 토큰"),
+    UNKNOWN_TOKEN(400, 2002, "알 수 없는 토큰"),
+    ;
+
+    private final Integer httpStatusCode;
+    private final Integer errorCode;
+    private final String description;
+}
--- api/src/main/java/org/delivery/api/domain/user/service/UserService.java
+++ api/src/main/java/org/delivery/api/domain/user/service/UserService.java
@@ -1,7 +1,7 @@
 package org.delivery.api.domain.user.service;
 
 import lombok.RequiredArgsConstructor;
-import org.delivery.api.common.error.ErrorCode;
+import org.delivery.api.common.error.UserErrorCode;
 import org.delivery.api.common.exception.ApiException;
 import org.delivery.db.user.UserEntity;
 import org.delivery.db.user.UserRepository;
@@ -25,18 +25,18 @@ public class UserService {
                     userEntity.setRegisteredAt(LocalDateTime.now());
                     return userEntity;
                 })
-                .orElseThrow(() -> new ApiException(ErrorCode.NULL_POINT, "User Entity Null"));
+                .orElseThrow(() -> new ApiException(UserErrorCode.USER_NOT_FOUND, "User Entity Null"));
 
         return userRepository.save(entity);
     }
 
     public UserEntity login(String email, String password) {
         return userRepository.findFirstByEmailAndPassword(email, password)
-                .orElseThrow(() -> new ApiException(ErrorCode.NULL_POINT, "유저가 없습니다."));
+                .orElseThrow(() -> new ApiException(UserErrorCode.USER_NOT_FOUND, "유저가 없습니다."));
     }
 
     public UserEntity getUser(String email, String password) {
     return userRepository.findFirstByEmailAndPasswordAndStatusOrderByIdDesc(email, password, UserStatus.REGISTERED)
-            .orElseThrow(() -> new ApiException(ErrorCode.NULL_POINT, "유저가 없습니다."));
+            .orElseThrow(() -> new ApiException(UserErrorCode.USER_NOT_FOUND, "유저가 없습니다."));
     }
 }
